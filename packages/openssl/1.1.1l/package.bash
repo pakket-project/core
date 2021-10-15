1fba06645996529d3e2afaf7e17a2becea7958fa
wouter <wouter@debruijn.dev>
2021-10-16 01:17:31 +0200

    feat: added postinstall script and updated package script

--- packages/openssl/1.1.1l/package.bash
+++ packages/openssl/1.1.1l/package.bash
@@ -2,7 +2,7 @@ cd openssl-1.1.1l
 
 unset OPENSSL_LOCAL_CONFIG_DIR
 
-./Configure --prefix="$PAKKET_PKG_PATH" "darwin64-$(uname -m)-cc" enable-ec_nistp_64_gcc_128
+./Configure --prefix="$PAKKET_PKG_PATH" no-ssl3 no-ssl3-method no-zlib "darwin64-$(uname -m)-cc" enable-ec_nistp_64_gcc_128
 make
 make install MANSUFFIX=ssl
 make test
