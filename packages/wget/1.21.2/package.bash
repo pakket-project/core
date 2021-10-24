773edddfcce76c20da1851b2fcd76021a6492a65
Sting Alleman <stingalleman@icloud.com>
2021-10-24 13:29:44 +0200

    fix: fix wget etc directory

--- packages/wget/1.21.2/package.bash
+++ packages/wget/1.21.2/package.bash
@@ -1,5 +1,4 @@
 cd wget-1.21.2 || exit
 
-#TODO add openssl flags
-./configure --prefix=$PAKKET_PKG_PATH --with-ssl=openssl --with-libssl-prefix=/usr/local --disable-pcre --disable-pcre2 --without-libpsl --without-included-regex
+./configure --prefix=$PAKKET_PKG_PATH --sysconfdir=/usr/local/etc --with-ssl=openssl --with-libssl-prefix=/usr/local --disable-pcre --disable-pcre2 --without-libpsl --without-included-regex
 make install
